0a1428c1250b1949a6c8c454019a37734fd429bd
Vitthal Sanadhya <77016445+vitthalSanadhya@users.noreply.github.com>
2025-09-26 00:08:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def search():
 	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
 
 if __name__ == "__main__":
-
-    app.run()
+	
+	app.run(host="0.0.0.0", port=5000, debug=False)
